0110364812a3c68f9571b4cc1e1489ef0ef075d9
Tokay <tokay@hell.(none)>
2010-04-11 23:31:02 +0300

    add tracks scaffold

--- website/config/routes.rb
+++ website/config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :tracks
+
 
   map.signup  '/signup', :controller  => 'users', :action => 'new'
   map.login   '/login', :controller => 'sessions', :action => 'new'
