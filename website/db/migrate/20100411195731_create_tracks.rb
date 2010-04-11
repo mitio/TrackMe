18cfa1de1295728a5023ef50adcf3c690a128517
Tokay <tokay@hell.(none)>
2010-04-11 23:51:30 +0300

    add files that I missed to add on the previous push

---
+++ website/db/migrate/20100411195731_create_tracks.rb
@@ -0,0 +1,16 @@
+class CreateTracks < ActiveRecord::Migration
+  def self.up
+    create_table :tracks do |t|
+      t.intteger :user
+      t.string :name
+      t.text :description
+      t.boolean :is_public, :null => false
+      t.decimal :rating, :precision => 2, :scale => 2
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :tracks
+  end
+end
